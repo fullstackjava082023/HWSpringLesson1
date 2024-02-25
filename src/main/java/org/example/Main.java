bee3a6432ed3e3fcee2e49ce42873f25fd6fc9ed
shashkist <shashkist@gmail.com>
2024-02-25 18:59:24 +0200

    Added list with xml

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,20 +12,6 @@ public class Main {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application-context.xml");
 
         Restaurant starksAndSons = (Restaurant) applicationContext.getBean("restaurant");
-        SimpleMenu simpleMenu = (SimpleMenu) applicationContext.getBean("simpleMenu");
-        MenuItem burger = (MenuItem) applicationContext.getBean("burger");
-        MenuItem pizza = (MenuItem) applicationContext.getBean("pizza");
-        //Restaurant starksAndSons = new Restaurant();
-       // SimpleMenu simpleMenu = new SimpleMenu();
-       // MenuItem burger = new MenuItem("Burger", "Juicy beef patty on a sesame seed bun", 8.99);
-      //  MenuItem pizza = new MenuItem("Pizza", "Thin crust pizza with assorted toppings", 10.99);
-
-        //connecting everything
-        simpleMenu.addItem(burger);
-        simpleMenu.addItem(pizza);
-       // starksAndSons.setMenu(simpleMenu);
-      //  starksAndSons.setName("starksAndSons");
-
         starksAndSons.displayMenu();
 
     }
